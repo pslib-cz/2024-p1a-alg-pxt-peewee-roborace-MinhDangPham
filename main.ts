370d847f32aecf58db9c9d50c68fb5dc12d2e5b2
Minh Dang Pham <minh.pham.024@pslib.cz>
2025-06-12 12:31:52 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,26 +3,57 @@ type IRC = {
     c: DigitalPin,
     r: DigitalPin
 }
-type PinData = {
-    l: boolean,
-    c: boolean,
-    r: boolean
-}
 const IR: IRC = {
     l: DigitalPin.P14,
-    c: DigitalPin.P15,
-    r: DigitalPin.P13
+    c: DigitalPin.P13,
+    r: DigitalPin.P12
 }
 pins.setPull(IR.l, PinPullMode.PullNone);
 pins.setPull(IR.c, PinPullMode.PullNone);
 pins.setPull(IR.r, PinPullMode.PullNone);
-
-let data: PinData = {l: false, c: false, r: false};
 basic.forever(function () {
-    data.l = pins.digitalReadPin(IR.l) === 1;
-    data.c = pins.digitalReadPin(IR.c) === 1;
-    data.r = pins.digitalReadPin(IR.r) === 1;
-    console.log(data);
-
-    basic.pause(20)
+    let left = pins.digitalReadPin(IR.l)
+    let center = pins.digitalReadPin(IR.c)
+    let right = pins.digitalReadPin(IR.r)
 })
+/*
+radio.setGroup(61)
+let rychlost: number = -100
+radio.onReceivedValue(function(name: "speed", speed) {
+    rychlost = speed
+})
+*/
+function forward() {
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, -120)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, -120)
+}
+function turnLeft() {
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, -100)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, -50)
+}
+function turnRight() {
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, -50)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, -100)
+}
+function stop() {
+    PCAmotor.MotorStopAll()
+}
+
+basic.forever(function () {
+    let left = pins.digitalReadPin(IR.l)
+    let center = pins.digitalReadPin(IR.c)
+    let right = pins.digitalReadPin(IR.r)
+
+    if (center == 0 && left == 1 && right == 1) {
+        forward()
+    } else if (left == 0) {
+        turnLeft()
+    } else if (right == 0) {
+        turnRight()
+    } else {
+        stop()
+    }
+
+    basic.pause(10)
+})
+
