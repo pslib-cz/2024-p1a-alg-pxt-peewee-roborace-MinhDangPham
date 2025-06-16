fc325f7ddd4a52a029c80cc3ea0c876e30c5875d
Minh Dang Pham <minh.pham.024@pslib.cz>
2025-06-16 09:43:20 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -16,49 +16,51 @@ type IRC = {
     c: DigitalPin,
     r: DigitalPin
 }
+
 const IR: IRC = {
     l: DigitalPin.P14,
     c: DigitalPin.P13,
     r: DigitalPin.P12
 }
 
-pins.setPull(IR.l, PinPullMode.PullNone);
-pins.setPull(IR.c, PinPullMode.PullNone);
-pins.setPull(IR.r, PinPullMode.PullNone);
-
-basic.forever(function () {
-    let left = pins.digitalReadPin(IR.l)
-    let center = pins.digitalReadPin(IR.c)
-    let right = pins.digitalReadPin(IR.r)
-})
+pins.setPull(IR.l, PinPullMode.PullNone)
+pins.setPull(IR.c, PinPullMode.PullNone)
+pins.setPull(IR.r, PinPullMode.PullNone)
 
 function forward() {
     PCAmotor.MotorRun(PCAmotor.Motors.M1, -170)
     PCAmotor.MotorRun(PCAmotor.Motors.M4, -170)
 }
+
 function turnLeft() {
     PCAmotor.MotorRun(PCAmotor.Motors.M1, -100)
     PCAmotor.MotorRun(PCAmotor.Motors.M4, -50)
 }
+
 function turnRight() {
     PCAmotor.MotorRun(PCAmotor.Motors.M1, -50)
     PCAmotor.MotorRun(PCAmotor.Motors.M4, -100)
 }
+
 function stop() {
     PCAmotor.MotorStopAll()
 }
 
 basic.forever(function () {
-    let left = pins.digitalReadPin(IR.l)
-    let center = pins.digitalReadPin(IR.c)
-    let right = pins.digitalReadPin(IR.r)
+    if (start) {
+        let left = pins.digitalReadPin(IR.l)
+        let center = pins.digitalReadPin(IR.c)
+        let right = pins.digitalReadPin(IR.r)
 
-    if (center == 0 && left == 1 && right == 1) {
-        forward()
-    } else if (left == 0) {
-        turnLeft()
-    } else if (right == 0) {
-        turnRight()
+        if (center == 0 && left == 1 && right == 1) {
+            forward()
+        } else if (left == 0) {
+            turnLeft()
+        } else if (right == 0) {
+            turnRight()
+        } else {
+            stop()
+        }
     } else {
         stop()
     }
